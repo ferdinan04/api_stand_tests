c135fd22b03e0d725637e9f210477920bdcd81ee
ferdinan04 <ferdinan.rebaza@pucp.pe>
2024-02-05 19:50:49 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://359ee71b-94d0-4033-b065-2992bd8db80d.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,35 @@
+import configuration
+import requests
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+                        params={"count": 20})
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+
+
+response = get_users_table()
+print(response.status_code)
+
+'''
+response = post_new_user(data.user_body)
+print(response.json())
+response = get_users_table()
+response = post_products_kits(data.product_ids)
+response = post_new_user(data.user_body)
+print(response.headers)
+print(response.status_code) '''
+
+
+
